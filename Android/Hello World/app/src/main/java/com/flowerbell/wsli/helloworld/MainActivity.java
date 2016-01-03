098fba3c0cdf5ecb43c38d24d8c55dddfdaaca24
wsli <99927800@qq.com>
2016-01-03 19:08:35 +0800

    init android hello world

---
+++ Android/Hello World/app/src/main/java/com/flowerbell/wsli/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.flowerbell.wsli.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
